e4cb27649b2010a9332079cc0d5cc905fbd29318
vivek <vivek.mendhe.022@gmail.com>
2023-06-15 09:48:45 +0530

    Orgnizations module will be added.

---
+++ src/test/java/Organizations/OrganizationWithChemicalsTest.java
@@ -0,0 +1,11 @@
+package Organizations;
+
+import org.testng.annotations.Test;
+
+public class OrganizationWithChemicalsTest {
+
+	@Test
+	public void createOrganisationWithIndustryTest() {
+		// Your code here..!
+	}
+}
